741e72693ed865c4638b9c004320219b3334ac18
Raul Mariaci Neto <raul.mariaci@gmail.com>
2021-06-09 21:27:12 -0300

    detalhes do produto

--- site.php
+++ site.php
@@ -48,4 +48,17 @@ $app->get("/categories/:idcategory", function($idcategory){
 	]);
 });
 
+$app->get("/products/:desurl", function ($desurl){
+
+	$product =  new Product();
+	$product->getFromURL($desurl);
+
+	$page = new Page();
+	$page->setTpl("detalhes-produto", [
+		'product'=>$product->getValues(), 
+		'categories'=>$product->getCategories()
+	]);
+
+});
+
 ?>
