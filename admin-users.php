86ac0c7308a32244640fb5bd6976aad4d4e46171
Raul Mariaci Neto <raul.mariaci@gmail.com>
2021-06-23 19:58:13 -0300

    ajustes no usuário admin

--- admin-users.php
+++ admin-users.php
@@ -1,7 +1,63 @@
 <?php  
 use \Hcode\PageAdmin;
-use \Hcode\Model\User;
+use \Hcode\Model\User; 
 
+
+$app->get("/admin/users/:iduser/password", function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-password" , [
+		'user'=>$user->getValues(),
+		'msgError'=>User::getError(),
+		'msgSuccess'=>User::getSuccess()
+	]);
+
+});
+
+$app->post("/admin/users/:iduser/password", function($iduser) {
+
+	User::verifyLogin();
+
+	if(!isset($_POST['despassword']) || $_POST['despassword'] === '') {
+
+		User::setError("Preencha a nova senha. ");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+	}
+	if(!isset($_POST['despassword-confirm']) || $_POST['despassword-confirm'] === '') {
+
+		User::setError("Preencha a confirmação da nova senha. ");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+	}
+
+	if($_POST['despassword'] !== $_POST['despassword-confirm']) {
+
+		User::setError("Confirme corretamente as senhas. ");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+	}
+	
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->setPassword(User::getPasswordHash($_POST['despassword-confirm']));
+
+
+	User::setSuccess("Senha alterada com sucesso. ");
+	header("Location: /admin/users/$iduser/password");
+	exit;
+
+
+});
 
 $app->get('/admin/users', function() {
 	
@@ -120,5 +176,6 @@ $app->get('/admin/users/:iduser/delete', function($iduser) {
 	header("Location: /admin/users");
 	exit;
 });
+
 
 ?>
