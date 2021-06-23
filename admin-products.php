cfe09f3365e443ea60618e134a66501108be899d
Raul Mariaci Neto <raul.mariaci@gmail.com>
2021-06-23 19:16:01 -0300

    paginação dos pedidos no admin

--- admin-products.php
+++ admin-products.php
@@ -36,9 +36,6 @@ $app->get("/admin/products", function() {
 
  	}
 
-
-	$products = Product::listAll();
-
 	$page = new PageAdmin();
 
 	$page->setTpl("products", [
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -161,7 +161,7 @@ class Product extends Model{
 
     }
 
-     public static function getPage($page = 1, $itemsPerPage = 10)
+    public static function getPage($page = 1, $itemsPerPage = 10)
     {
         
         $start = ($page - 1) * $itemsPerPage;
